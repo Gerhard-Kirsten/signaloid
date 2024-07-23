5c9e6c69ae011ce9b993c59bb266bd778a8d260f
Gerhard-Kirsten <63722837+Gerhard-Kirsten@users.noreply.github.com>
2024-07-23 20:13:36 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -22,6 +22,21 @@ int main() {
     double airDensity = calculateAirDensity(temperatureC, pressurePa, humidity);
     printf("Calculated Air Density: %lf kg/m³\n", airDensity);
 
+	// Air velocity (V) in m/s
+   double V = UxHwDoubleUniformDist(45, 55);
+	printf("V = %lf\n", V);
+    
+    // Coefficient of lift (CL)
+   double CL = UxHwDoubleUniformDist(1.1,1.3);
+	printf("CL = %lf\n", CL);
+    
+    // Wing area (A) in m^2 (assumed to be known precisely)
+    double A = 10.0;
+
+    // Compute the lift force
+   double lift =  0.5 * airDensity * V * V * CL * A;
+	printf("lift = %lf\n", lift);
+
     return 0;
 }
 
