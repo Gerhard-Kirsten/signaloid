d8e5266e1f1a041b20669bc193283c733b09bbab
Gerhard-Kirsten <63722837+Gerhard-Kirsten@users.noreply.github.com>
2024-07-23 19:03:39 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,12 +1,70 @@
 #include <stdio.h>
 #include <uxhw.h>
+#include <stdint.h>
+#include <uncertain.h>
+
+double empiricalTaylorFactorValues[] = {
+						3.2,
+						3.9,
+						4.1,
+						3.2,
+						3.8,
+						3.8,
+						2.1,
+						3.0,
+						1.9,
+						3.9,
+						2.3,
+						2.2,
+						3.2,
+						2.2,
+						3.9,
+						2.2,
+						1.9,
+						3.2,
+						3.9,
+						3.1,
+					};
+
+	*M		= libUncertainDoubleDistFromSamples(
+								empiricalTaylorFactorValues,
+								sizeof(empiricalTaylorFactorValues)/sizeof(double)
+							);
 
 int
 main(int argc, char *  argv[])
 {
 	double	a, b, c;
 
-	a = UxHwDoubleUniformDist(0.5, 1.0);
+	double empiricalTaylorFactorValues[] = {
+						3.2,
+						3.9,
+						4.1,
+						3.2,
+						3.8,
+						3.8,
+						2.1,
+						3.0,
+						1.9,
+						3.9,
+						2.3,
+						2.2,
+						3.2,
+						2.2,
+						3.9,
+						2.2,
+						1.9,
+						3.2,
+						3.9,
+						3.1,
+					};
+
+	a		= libUncertainDoubleDistFromSamples(
+								empiricalTaylorFactorValues,
+								sizeof(empiricalTaylorFactorValues)/sizeof(double)
+							);
+
+	#a = UxHwDoubleUniformDist(0.5, 1.0);
 	printf("a = %lf\n", a);
 
 	b = UxHwDoubleUniformDist(10.0, 20.0);
